9921a76d913dcfd5a318b1e6822c9cc4abce5a4c
gamadv <lealdev.py@gmail.com>
2022-11-25 16:19:58 -0300

    Remove unused template content

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GlobalContextProvider } from "./context/GlobalContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalContextProvider>
-      <App />
-    </GlobalContextProvider>
+    <App />
   </React.StrictMode>
 );
