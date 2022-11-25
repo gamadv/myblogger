a46dbad18aeb8849be0e2b950c87e22884755a8e
gamadv <lealdev.py@gmail.com>
2022-11-24 23:39:20 -0300

    feat: Create api base and Blog Service file

---
+++ src/services/blogService.ts
@@ -0,0 +1,51 @@
+import { api } from './api'
+
+export type TBlog = {
+    userId: number,
+    id: number,
+    title: string
+    body: string
+}
+export type TUser = {
+    id: number,
+    name: string
+    username: string
+    email: string
+    address: {
+        street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: string
+            lng: string
+        }
+    },
+    phone: string
+    website: string
+    company: {
+        name: string
+        catchPhrase: string
+        bs: string
+    }
+}
+
+const blogEndpointPath = {
+    postList: '/posts',
+    userList: '/users'
+
+}
+
+async function FetchBlogList(id?: number) {
+    const parseUrl = id ? `${blogEndpointPath.postList}/${id}` : blogEndpointPath.postList
+
+    return api.get<TBlog[]>(parseUrl)
+}
+async function FetchUserList(id?: number) {
+    const parseUrl = id ? `${blogEndpointPath.userList}/${id}` : blogEndpointPath.userList
+
+    return api.get<TUser[]>(parseUrl)
+}
+
+
+export { FetchBlogList, FetchUserList }
