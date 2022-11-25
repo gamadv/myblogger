b71a1f39bf7536c4230e97205d6e9c8f837d1fc2
gamadv <lealdev.py@gmail.com>
2022-11-24 23:22:32 -0300

    Tailwind and Meronex added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require("path");
+
+module.exports = {
+  content: [
+    join(__dirname, "./pages/**/*.{js,ts,jsx,tsx}"),
+    join(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
+  ],
+  plugins: [require("@tailwindcss/line-clamp"), require("tailwind-scrollbar")],
+
+  theme: {
+    extend: {},
+  },
+};
