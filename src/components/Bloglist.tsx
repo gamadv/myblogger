a5020dda8542175e10f92fef797676dfeab6f0c7
gamadv <lealdev.py@gmail.com>
2022-11-28 19:08:11 -0300

    feat: Bloglist component finished

--- src/components/Bloglist.tsx
+++ src/components/Bloglist.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { TBlog } from "../services/blogService";
 import LoadingSpinner from "./LoadingSpinner";
+import PostItem, { TPost } from "./PostItem";
 
 type TBlogList = {
   postList: TBlog[] | undefined;
 };
-const postsToShow = 4;
+
+const postsToShow = 6;
 
 export default function Bloglist({ postList }: TBlogList) {
   const [ListQuantity, setUpdateListQuantity] = useState(postsToShow);
+  const [postItemState, setUpdateListItemState] = useState<TPost>();
+  const [showPostItem, setShowPostItem] = useState(false);
 
   const hasList = postList && postList?.length > 0;
 
@@ -16,31 +20,62 @@ export default function Bloglist({ postList }: TBlogList) {
     setUpdateListQuantity(ListQuantity + postsToShow);
   }
 
+  function handleOpenPost(
+    postId: number,
+    postTitle: string,
+    postBody: string,
+    userId: number
+  ) {
+    setUpdateListItemState({ postBody, postId, postTitle, userId });
+    handleTogglePost();
+  }
+
+  function handleTogglePost() {
+    setShowPostItem((oldState) => !oldState);
+  }
+
   return hasList ? (
-    <div className="flex justify-center flex-col">
-      <ul className="flex flex-wrap">
-        {postList?.slice(0, ListQuantity).map(({ body, id, title, userId }) => {
-          return (
-            <li key={id}>
-              <article className="bg-[#344D67] rounded min-w-[300px] w-[100%] max-w-[600px] min-h-[200px] max-h-[200px] line-clamp-5 m-5 p-4 cursor-pointer">
-                <header className="bg-slate-50 flex items-start justify-center text-center">
-                  {title}
-                </header>
-                <p className="mt-3 text-yellow-100">{body}</p>
-              </article>
-            </li>
-          );
-        })}
-      </ul>
-      {ListQuantity < postList?.length && (
-        <button
-          className="mt-4 p-2 bg-slate-100 text-[#344D67] max-w-[150px] self-center"
-          onClick={handleMorePosts}
-        >
-          Mostrar mais +
-        </button>
+    <>
+      <div className="flex justify-center flex-col">
+        <ul className="flex flex-wrap">
+          {postList
+            ?.slice(0, ListQuantity)
+            .map(({ body, id, title, userId }) => {
+              return (
+                <li key={id}>
+                  <article
+                    className="bg-[#344D67] rounded max-w-sm min-h-[200px] h-[100%] max-h-[200px] m-5 p-4 cursor-pointer"
+                    onClick={() => handleOpenPost(id, title, body, userId)}
+                  >
+                    <header className="bg-slate-50 flex items-start justify-center text-center">
+                      {title}
+                    </header>
+                    <p className="mt-3 text-yellow-100 line-clamp-4 ">{body}</p>
+                  </article>
+                </li>
+              );
+            })}
+        </ul>
+        {ListQuantity < postList?.length && (
+          <button
+            className="mt-4 p-2 bg-slate-100 text-[#344D67] max-w-[150px] self-center"
+            onClick={handleMorePosts}
+          >
+            Mostrar mais +
+          </button>
+        )}
+      </div>
+      {showPostItem && (
+        <PostItem
+          postId={postItemState?.postId}
+          postTitle={postItemState?.postTitle}
+          postBody={postItemState?.postBody}
+          userId={postItemState?.userId}
+          showPostItem={showPostItem}
+          handleClosePost={handleTogglePost}
+        />
       )}
-    </div>
+    </>
   ) : (
     <LoadingSpinner />
   );
